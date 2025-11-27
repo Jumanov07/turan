9ff7f14ed117fbf2d3ac0158efd856942230fc96
Alisher <zumanovaliser814@gmail.com>
2025-11-27 17:29:48 +0600

    routes

--- src/app/router/ui/protected-route/index.tsx
+++ src/app/router/ui/protected-route/index.tsx
@@ -9,17 +9,23 @@ interface ProtectedRouteProps {
 
 export const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { user, accessToken } = useAuthStore();
+
   const location = useLocation();
 
-  if (!accessToken) return <Navigate to="/sign-in" replace />;
+  if (!accessToken) {
+    return <Navigate to="/sign-in" replace />;
+  }
 
-  if (!user) return null;
+  if (!user) {
+    return null;
+  }
 
   const allowedPaths = getAllowedPathsByRole(user.role);
-
-  if (allowedPaths.length === 0) return <Navigate to="/sign-in" replace />;
-
   const firstAllowed = allowedPaths[0];
+
+  if (allowedPaths.length === 0) {
+    return <Navigate to="/sign-in" replace />;
+  }
 
   if (!allowedPaths.includes(location.pathname)) {
     return <Navigate to={firstAllowed} replace />;
